101c8310d9289c3a51eabc8d993e3d50dec130ed
Abhishek Mishra <abhishek@charlee.ai>
2023-10-27 12:35:49 +0530

    chore: rerun cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo su - test3
-cd /home2/test3/UI
-ticket_id=${{ steps.extract-ticket-id.outputs.ticket_id }}
+sudo su - test3 -c id
+sudo su - test3 -c pwd
 echo "Ticket ID: $ticket_id"
-mkdir $ticket_id
-cd $ticket_id
-ls
+sudo su - test3 -c "mkdir UI/$ticket_id"
+sudo su - test3 -c "cd UI/$ticket_id"
+sudo su - test3 -c "ls UI/"
+
 start_port=3000
 end_port=9000
 
@@ -18,3 +18,4 @@ do
     break  # Exit the loop after finding an available port
     fi
 done
+#     ./start_server.sh $AVAILABLE_PORT
