e3c2ad42542eda551a1fca6998b451ae3e6f1684
Abhishek Mishra <abhishek@charlee.ai>
2023-11-15 12:27:17 +0530

    chore: continous deployment workflow, please deploy to port 4002

---
+++ scripts/notify.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Default values for variables
+REPOSITORY_NAME="icvs_pirv"
+SLACK_WEBHOOK_URL=""
+TICKET_ID=""
+DEPLOYMENT_STATUS=""
+DEPLOYMENT_LOCATION=""
+BRANCH_NAME=""
+
+# Parse command-line arguments
+while getopts "s:w:t:b:d:" opt; do
+  case $opt in
+    s)
+      DEPLOYMENT_STATUS="$OPTARG"
+      ;;
+    w)
+      SLACK_WEBHOOK_URL="$OPTARG"
+      ;;
+    t)
+      TICKET_ID="$OPTARG"
+      ;;
+    b)
+      BRANCH_NAME="$OPTARG"
+      ;;
+    d)
+      DEPLOYMENT_LOCATION="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Validate required arguments
+if [ -z "$SLACK_WEBHOOK_URL" ]; then
+  echo "SLACK_WEBHOOK_URL is required. Use the -w flag to specify it."
+  exit 1
+fi
+
+# Set the default icon to exclamation
+ICON_EMOJI=":bell:"
+
+# If the status is not failure, set the icon to bell
+if [ "${DEPLOYMENT_STATUS}" == "failure" ]; then
+    ICON_EMOJI=":exclamation:"
+fi
+
+echo $ICON_EMOJI
+
+if [ "${DEPLOYMENT_STATUS}" = "failure" ]; then
+  SLACK_MESSAGE="Repository Name :- ${REPOSITORY_NAME} \n Ticket ID :- ${TICKET_ID} \n Branch:- ${BRANCH_NAME} \n Deployment Status :- ${DEPLOYMENT_STATUS}:bangbang:"
+else
+  SLACK_MESSAGE="Repository Name :- ${REPOSITORY_NAME} \n Ticket ID :- ${TICKET_ID} \n Branch:- ${BRANCH_NAME} \n Deployment Status :- ${DEPLOYMENT_STATUS} \n Deployment Location :- ${DEPLOYMENT_LOCATION}"
+fi
+
+# Create Slack payload
+PAYLOAD='{
+  "channel": "dev-qa-team",
+  "icon_emoji": "'"${ICON_EMOJI}"'",
+  "username": "deployment-bot",
+  "attachments": [
+    {
+      "color": "'"${DEPLOYMENT_STATUS}"'",
+      "title": "Deployment alert",
+      "text": "'"${SLACK_MESSAGE}"'"
+    }
+  ]
+}'
+
+# Send the notification to Slack using curl
+curl -X POST -H 'Content-type: application/json' --data "${PAYLOAD}" "${SLACK_WEBHOOK_URL}"
